d9c6add7768d20f119f4c4b2e99bf0ab78348330
RuiYuriAfricano <yuriafricano03@gmail.com>
2023-07-02 17:03:15 +0100

    feat: adjustment utec-api

--- utec-api/src/app.module.ts
+++ utec-api/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { ActorModule } from './actor/actor.module';
+import { ClienteModule } from './cliente/cliente.module';
+import { MotoristaModule } from './motorista/motorista.module';
+import { MotoristaTaxiModule } from './motorista-taxi/motoristaTaxi.module';
+import { TaxiModule } from './taxi/taxi.module';
+import { ViagemModule } from './viagem/viagem.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [ActorModule, ClienteModule, MotoristaModule, MotoristaTaxiModule, TaxiModule, ViagemModule],
+  providers: [PrismaService],
 })
 export class AppModule { }
